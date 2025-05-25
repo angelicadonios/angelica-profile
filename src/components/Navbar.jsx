ee8e0f914c4ed41b85d5667bf895b840f294f617
Angelica Donios <angelica.donios.tbs@gmail.com>
2025-05-25 17:11:32 +0800

    logo and accent color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon aria-hidden="true" className="block size-6 group-data-[open]:hidden" />
@@ -44,8 +44,8 @@ const Navbar = () => {
                     href={item.href}
                     aria-current={item.current ? 'page' : undefined}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:text-white',
-                      'rounded-md px-3 py-2 text-sm font-medium',
+                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300',
+                      'rounded-md px-3 py-2 text-sm font-medium nav-highlight',
                     )}
                   >
                     {item.name}
@@ -55,13 +55,13 @@ const Navbar = () => {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <a href="mailto:angelicadonios@gmail.com" target="_blank" className="relative bg-slate-900 p-1 text-gray-400 hover:text-white">
+            <a href="mailto:angelicadonios@gmail.com" target="_blank" className="relative bg-slate-900 p-1 text-gray-400 nav-highlight">
               <EnvelopeIcon aria-hidden="true" className="size-5" />
             </a>
-            <a href="https://www.linkedin.com/in/angelica-donios/" target="_blank" className="relative bg-slate-900 p-1 text-gray-400 hover:text-white">
+            <a href="https://www.linkedin.com/in/angelica-donios/" target="_blank" className="relative bg-slate-900 p-1 text-gray-400 nav-highlight">
               <AtSymbolIcon aria-hidden="true" className="size-5" />
             </a>
-            <a href="https://github.com/angelicadonios" target="_blank" className="relative bg-slate-900 p-1 text-gray-400 hover:text-white">
+            <a href="https://github.com/angelicadonios" target="_blank" className="relative bg-slate-900 p-1 text-gray-400 nav-highlight">
               <CodeBracketIcon aria-hidden="true" className="size-5" />
             </a>
             {/* <a href="https://medium.com/@snowiangel" target="_blank" className="relative bg-slate-900 p-1 text-gray-400 hover:text-white">
@@ -81,8 +81,8 @@ const Navbar = () => {
               href={item.href}
               aria-current={item.current ? 'page' : undefined}
               className={classNames(
-                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium',
+                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700',
+                'block rounded-md px-3 py-2 text-base font-medium nav-highlight',
               )}
             >
               {item.name}
